bafff483c8b3b66b4199280f9c8238cb28c315b3
minerva421 <minerva421.ai@gmail.com>
2023-11-08 02:42:34 +0900

    TodoItem 수정

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
+import { MdDone, MdDelete } from 'react-icons/md';
+
+const Remove = styled.div`
+  display: none;
+  align-items: center;
+  justify-content: center;
+  color: #dee2e6;
+  font-size: 24px;
+  cursor: pointer;
+  &:hover {
+    color: #ff6b6b;
+  }
+`;
 
 const TodoItemBlock = styled.div`
   display: flex;
   align-items: center;
   padding-top: 12px;
   padding-bottom: 12px;
+  &:hover {
+    ${Remove} {
+      display: initial;
+    }
+  }
 `;
 
 const CheckCircle = styled.div`
@@ -19,6 +37,12 @@ const CheckCircle = styled.div`
   justify-content: center;
   margin-right: 20px;
   cursor: pointer;
+  ${(props) =>
+    props.done &&
+    css`
+      border: 1px solid #38d9a9;
+      color: #38d9a9;
+    `}
 `;
 
 const Text = styled.div`
@@ -34,9 +58,12 @@ const Text = styled.div`
 
 function TodoItem({ text, done }) {
   return (
-    <TodoItemBlock $done={done}>
-      <CheckCircle></CheckCircle>
-      <Text $done={done}>{text}</Text>
+    <TodoItemBlock>
+      <CheckCircle done={done}>{done && <MdDone />}</CheckCircle>
+      <Text done={done}>{text}</Text>
+      <Remove>
+        <MdDelete />
+      </Remove>
     </TodoItemBlock>
   );
 }
